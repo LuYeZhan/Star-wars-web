8126e747ef2e2716d5ebea9251b464ec6485c190
Lu <lu@hooli.store>
2019-07-09 15:21:40 +0200

    webapi page finished, transition between pages using AP's service, router and layout

--- Layout.js
+++ Layout.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function Layout(root, style) {
+  this.root = root;
+  this.style = style;
+  this.elements = null;
+  this.header = null;
+  this.main = null;
+  this.footer = null;
+}
+
+Layout.prototype.generate = function(){
+  // generar los elementos del DOM
+  this.elements = `
+    <header id="site-header"></header>
+    <main id="site-main"></main>
+    <footer id="site-footer"></footer>
+  `;
+  this.render();
+  this.getContainers();
+}
+
+Layout.prototype.render = function() {
+  // añadir los elementos al DOM
+  this.root.innerHTML = this.elements;
+}
+
+Layout.prototype.getContainers = function(){
+  // get header, main, footer del DOM
+  this.header = document.querySelector('#site-header');
+  this.main = document.querySelector('#site-main');
+  this.footer = document.querySelector('#site-footer');
+}
--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 'use strict';
+
+function main (){
+  var ENTRY_POINT = '/';
+  var layoutInstance = null;
+  var navbarInstance = null;
+  var rootElement = document.querySelector('#root');
+  var links = [
+    {name: 'Home',
+    url: '/',
+    },
+    {name: 'Movies',
+    url: '/movies',
+    }
+  ];
+
+  generateLayout();
+  generateNavbar();
+  addListenersToNavBar();
+  activateRouter();
+  
+
+  function generateLayout(){
+    layoutInstance = new Layout(rootElement);
+    layoutInstance.generate();
+  }
+
+  function generateNavbar(){
+    navbarInstance = new Navbar(layoutInstance.header, links);
+    navbarInstance.generate();
+  }
+  function activateRouter(){
+    routerInstance.buildDOM(ENTRY_POINT, layoutInstance.main);
+  }
+  function addListenersToNavBar(){
+    var anchors = document.querySelectorAll('nav a');
+    anchors.forEach(function(anchor){
+      anchor.addEventListener('click', changePage)
+    })
+  }
+function changePage(event){
+  // console.dir sirve para ver el objeto y todas sus propiedades de lo que esta dentro del console
+  // console.dir(event.target.attributes.url.value);
+  var url = event.target.attributes.url.value;
+  routerInstance.buildDOM(url, layoutInstance.main);
+}
+
+};
+
+window.addEventListener('load',main);
